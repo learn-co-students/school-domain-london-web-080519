1b726d0284975aa4ba559544d0ac9bbd4e2e7edc
github username <github email address>
2019-08-06 16:28:31 +0100

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
 # code here!
+
+class School
+    def initialize(roster)
+        @roster = Hash.new {|grade,student| grade[student]=[]}
+    end
+
+    attr_reader :roster
+
+    def add_student(student, grade)
+        @roster[grade] << student
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each do |grade, student|
+            student.sort!
+        end
+    end
+end
