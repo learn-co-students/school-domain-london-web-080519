23fffc880529cdd1144ecca118e6cee7d9066b11
PollyCR <polly.c.reynolds@gmail.com>
2019-08-06 15:13:36 +0100

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,9 @@ class School
 
     
     def add_student(name,grade)
+         
         roster[grade]||= []
+        
         roster[grade] << name
     end
     def grade(level)
@@ -33,20 +35,5 @@ end
 
   
 
+ 
   
-  #Use grade levels as keys 
-  
-  #Use names as values
-  
-  #create an add student method with arguments of name and grade 
-  
-  #add student to grade level key 
-  #if key doesn't exist yet, create it 
-  
-  #point it to an empty value array 
-  
-  #if value array exists, add new value to it
-  
-  #Define new method accepting integer argument
-  
-  #return hash[:grade_level] if hash[:grade_level] == integer
